856aa3226a92c7f2069b79b3ca4816fd1b1e4390
Sanaxen <bccatfalcon@gmail.com>
2021-07-03 13:17:06 +0900

    rename

---
+++ tsu_dataset/CREATE_DATA/データ作成(宝永).bat
@@ -0,0 +1,42 @@
+set ldm=..\\project_and_run\bin\bitmapedit.exe
+
+del tmp\*.* /Q
+del シミュレレーション用入力データ\*.* /Q
+
+copy .\地形データ\E100N40\*.* tmp /v /y
+
+set SX=3356
+set EX=4766
+set SY=464
+set EY=1100
+
+%ldm% -clip %SX% %EX% %SY% %EY% tmp\aa.bmp
+copy cliped.bmp aa_cliped.bmp
+
+%ldm% -clip %SX% %EX% %SY% %EY% tmp\bb.bmp
+copy cliped.bmp bb_cliped.bmp
+
+%ldm% -clip %SX% %EX% %SY% %EY% tmp\cc.bmp
+copy cliped.bmp cc_cliped.bmp
+
+%ldm% -clip_tr %SX% %EX% %SY% %EY% tmp\E100N40.raw
+copy cliped.csv シミュレレーション用入力データ\elevation_data2.csv
+
+:%ldm% -clip_tr %SX% %EX% %SY% %EY% tmp\E100N40_.raw
+:copy cliped.csv シミュレレーション用入力データ\water_depth_data2.csv
+
+
+%ldm% -e7_t tmp\e100n40_point.raw lon.raw lat.raw hi.raw
+ 
+%ldm% -clip_tr %SX% %EX% %SY% %EY% hi.raw
+copy cliped.csv シミュレレーション用入力データ\water_depth_data2.csv
+
+%ldm% -clip_tr %SX% %EX% %SY% %EY% lon.raw "LON"
+copy parameter.txt lon.txt /v /y
+%ldm% -clip_tr %SX% %EX% %SY% %EY% lat.raw "LAT"
+copy parameter.txt lat.txt /v /y
+
+type lon.txt > シミュレレーション用入力データ\経度緯度情報.txt
+type lat.txt >> シミュレレーション用入力データ\経度緯度情報.txt
+
+call _データ作成.bat
---
+++ tsu_dataset/CREATE_DATA/データ作成(東北太平洋沖)new.bat
@@ -0,0 +1,44 @@
+set ldm=..\\project_and_run\bin\bitmapedit.exe
+
+del tmp\*.* /Q
+del シミュレレーション用入力データ\*.* /Q
+
+copy .\地形データ\E140N40-90\*.* tmp /v /y
+
+set SX=1
+set EX=860
+set SY=5309
+set EY=6610
+
+
+%ldm% -clip %SX% %EX% %SY% %EY% tmp\aa.bmp
+copy cliped.bmp aa_cliped.bmp
+
+%ldm% -clip %SX% %EX% %SY% %EY% tmp\bb.bmp
+copy cliped.bmp bb_cliped.bmp
+
+%ldm% -clip %SX% %EX% %SY% %EY% tmp\cc.bmp
+copy cliped.bmp cc_cliped.bmp
+
+%ldm% -clip_tr %SX% %EX% %SY% %EY% tmp\E140N40-90.raw
+copy cliped.csv シミュレレーション用入力データ\elevation_data2.csv
+
+:%ldm% -clip_tr %SX% %EX% %SY% %EY% tmp\E140N40-90_.raw
+:copy cliped.csv シミュレレーション用入力データ\water_depth_data2.csv
+
+
+%ldm% -e7_t tmp\E140N40-90_point.raw lon.raw lat.raw hi.raw
+ 
+%ldm% -clip_tr %SX% %EX% %SY% %EY% hi.raw
+copy cliped.csv シミュレレーション用入力データ\water_depth_data2.csv
+
+%ldm% -clip_tr %SX% %EX% %SY% %EY% lon.raw "LON"
+copy parameter.txt lon.txt /v /y
+
+%ldm% -clip_tr %SX% %EX% %SY% %EY% lat.raw "LAT"
+copy parameter.txt lat.txt /v /y
+
+type lon.txt > シミュレレーション用入力データ\経度緯度情報.txt
+type lat.txt >> シミュレレーション用入力データ\経度緯度情報.txt
+
+call _データ作成.bat
