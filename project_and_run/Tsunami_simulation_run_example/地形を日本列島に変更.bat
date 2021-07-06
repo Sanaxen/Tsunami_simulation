0d3edafc7be7e7776af0ac3d8bf3ef3eea24c4f5
Sanaxen <bccatfalcon@gmail.com>
2021-07-06 22:46:53 +0900

    Create 地形を日本列島に変更.bat

---
+++ project_and_run/Tsunami_simulation_run_example/地形を日本列島に変更.bat
@@ -0,0 +1,9 @@
+
+copy "..\..\tsu_dataset\tu_sample\日本列島"\*.* /v/ y
+
+@echo CalculationParameters.txt のパラメータを変更して下さい
+@echo CalculationParameters.txt内の LAT, LONの部分を
+@echo 経度緯度情報.txt を転記するだけです。
+
+pause
+
