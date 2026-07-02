c5c45f8f1066c99c3f604cc41f323159840d2076
Sanaxen <bccatfalcon@gmail.com>
2026-07-02 13:30:47 +0900

    -

--- project_and_run/bin/cnv_move2D.bat
+++ project_and_run/bin/cnv_move2D.bat
@@ -1,8 +1,9 @@
 set PATH=%PATH%;%~dp0
 
 :ffmpeg -b 5000 -y -i image\output_W%%06d.bmp -speed 0.01 -qscale 7 simulation.swf
-ffmpeg  -r 6 -y -i image\output_W%%06d.bmp -codec msmpeg4v2 -qscale 7 simulation.avi
-ffmpeg  -r 6 -y -i image\output_W%%06d.bmp simulation.gif
+:ffmpeg  -r 6 -y -i image\output_W%%06d.bmp -codec msmpeg4v2 -qscale 7 simulation.avi
+:ffmpeg  -r 6 -y -i image\output_W%%06d.bmp simulation.gif
 
-
+ffmpeg  -y  -start_number 0 -i image\output_W%%06d.bmp -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:v libx264 -pix_fmt yuv420p simulation.mp4
+ffmpeg -y -r 5 -i simulation.mp4 -filter_complex "[0:v] fps=20,scale=608:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" simulation.gif
 
--- project_and_run/bin/cnv_move3D.bat
+++ project_and_run/bin/cnv_move3D.bat
@@ -1,7 +1,10 @@
 set PATH=%PATH%;%~dp0
 
 :ffmpeg.exe -y  -r 6 -i image3D\output_%%06d.bmp  -qscale 6 -b 500k simulation3D_%1.swf
-ffmpeg -r 8 -y -i image3D\output_%%06d.bmp  -codec msmpeg4v2 -qscale 7 simulation3D.avi
-ffmpeg -r 8 -i image3D\output_%%06d.bmp -vcodec libx264  -pix_fmt yuv420p simulation3D.mp4
+:ffmpeg -r 8 -y -i image3D\output_%%06d.bmp  -codec msmpeg4v2 -qscale 7 simulation3D.avi
+:ffmpeg -r 8 -i image3D\output_%%06d.bmp -vcodec libx264  -pix_fmt yuv420p simulation3D.mp4
 
 :ffmpeg -r 8 -y -i image3D\output_%%06d.bmp  -codec msmpeg4v2 -q 0 simulation3D.avi
+
+ffmpeg  -r 8 -y  -start_number 0 -i image3D\output_%%06d.bmp -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:v libx264 -pix_fmt yuv420p simulation3D.mp4
+ffmpeg -y -r 5 -i simulation3D.mp4 -filter_complex "[0:v] fps=20,scale=608:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" simulation3D.gif
