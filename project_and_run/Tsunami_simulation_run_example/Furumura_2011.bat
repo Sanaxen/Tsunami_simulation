526dde1da43fc7d3c3fb6014ab92e07d2493c4af
Sanaxen <bccatfalcon@gmail.com>
2026-07-02 10:33:42 +0900

    -

--- project_and_run/Tsunami_simulation_run_example/Furumura_2011.bat
+++ project_and_run/Tsunami_simulation_run_example/Furumura_2011.bat
@@ -2,7 +2,7 @@ call init.bat
 call clear.bat
 call ïÛâiínêkv2.bat
 
-copy ..\tu_sample\Furumura_2011\*.* /v/ y
+copy ..\..\tu_sample\Furumura_2011\*.* /v/ y
 
 ..\bin\faultDefromationSolv.exe CalculationParameters.txt
 
