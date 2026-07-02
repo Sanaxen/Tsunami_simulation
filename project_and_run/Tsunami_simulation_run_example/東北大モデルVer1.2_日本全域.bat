cec57726ba4f72ae3efebd76333d3c6c9b0913d3
Sanaxen <bccatfalcon@gmail.com>
2026-07-02 13:31:08 +0900

    Update 東北大モデルVer1.2_日本全域.bat

--- project_and_run/Tsunami_simulation_run_example/東北大モデルVer1.2_日本全域.bat
+++ project_and_run/Tsunami_simulation_run_example/東北大モデルVer1.2_日本全域.bat
@@ -3,7 +3,7 @@ del Initial_wave_data*.bmp
 del Initial_wave_data*.csv
 del FaultParameters*.bmp
 
-copy .\tu_sample\日本列島\*.* /v/ y
-copy ".\tu_sample\東北大モデルVer1.2_日本全域"\*.* /v/ y
+copy ..\..\tsu_dataset\tu_sample\日本列島\*.* /v/ y
+copy ..\..\tsu_dataset\tu_sample\東北大モデルVer1.2_日本全域\*.* /v/ y
 
 Release\faultDefromationSolv.exe CalculationParameters.txt
